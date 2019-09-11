a583455c097c1ac36e59df3af3a6f4dcc5adf3f7
dylanreinsma <dylanreinsma@gmail.com>
2019-09-11 10:22:40 -0700

    updates

--- app/src/main/java/com/dylanreinsma/notes/NoteActivity.java
+++ app/src/main/java/com/dylanreinsma/notes/NoteActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.dylanreinsma.notes.models.Note;
 
@@ -11,10 +13,20 @@ public class NoteActivity extends AppCompatActivity {
 
     private static final String TAG = "NoteActivity";
 
+    private LinedEditText linedEditText;
+    private EditText editText;
+    private TextView textView;
+
+    private boolean isNewNote;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+        linedEditText = findViewById(R.id.noteText);
+        editText = findViewById(R.id.noteTextTitle);
+        textView = findViewById(R.id.note_title);
 
         if (getIntent().hasExtra("selected_note")) {
 

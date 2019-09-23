8a87b2935a85127488c6387f5ca743411d6b4b25
dylanReinsma <dylanreinsma@gmail.com>
2019-09-23 12:15:20 -0700

    adding dao

--- app/src/main/java/com/dylanreinsma/notes/models/Note.java
+++ app/src/main/java/com/dylanreinsma/notes/models/Note.java
@@ -3,9 +3,23 @@ package com.dylanreinsma.notes.models;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "notes")
 public class Note implements Parcelable {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "title")
     private String title;
+
+    @ColumnInfo(name = "content")
     private String content;
+
+    @ColumnInfo(name = "timestamp")
     private String timestamp;
 
     public Note(String title, String content, String timestamp) {
@@ -14,26 +28,16 @@ public class Note implements Parcelable {
         this.timestamp = timestamp;
     }
 
+    @Ignore
     public Note() {
 
     }
 
     protected Note(Parcel in) {
+        id = in.readInt();
         title = in.readString();
         content = in.readString();
         timestamp = in.readString();
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(title);
-        dest.writeString(content);
-        dest.writeString(timestamp);
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
     }
 
     public static final Creator<Note> CREATOR = new Creator<Note>() {
@@ -47,6 +51,14 @@ public class Note implements Parcelable {
             return new Note[size];
         }
     };
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
@@ -75,9 +87,23 @@ public class Note implements Parcelable {
     @Override
     public String toString() {
         return "Note{" +
-                "title='" + title + '\'' +
+                "id=" + id +
+                ", title='" + title + '\'' +
                 ", content='" + content + '\'' +
                 ", timestamp='" + timestamp + '\'' +
                 '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(title);
+        dest.writeString(content);
+        dest.writeString(timestamp);
     }
 }
---
+++ app/src/main/java/com/dylanreinsma/notes/persistence/NoteDao.java
@@ -0,0 +1,31 @@
+package com.dylanreinsma.notes.persistence;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.dylanreinsma.notes.models.Note;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+
+    @Insert
+    long[] insertNotes(Note... notes);
+
+    @Query("SELECT * FROM notes")
+    LiveData<List<Note>> getNotes();
+
+    @Query("SELECT * FROM notes WHERE title LIKE :title")
+    List<Note> getNotesWithCustomQuery(String title);
+
+    @Delete
+    int delete(Note... notes);
+
+    @Update
+    int update(Note... notes);
+}
